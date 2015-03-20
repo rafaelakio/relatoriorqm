5e971a03a1c1c52c252440d0f19b54ef911e071f
rcoelho <rcoelho@9.78.149.5>
2015-03-20 18:35:57 -0300

    criacao do projeto relatorio rqm

---
+++ src/br/ibm/com/excel/Relatorio.java
@@ -0,0 +1,13 @@
+package br.ibm.com.excel;
+
+import com.ibm.rqm.ct.CopyUtil;
+
+public class Relatorio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CopyUtil.main(args);
+	}
+
+}
